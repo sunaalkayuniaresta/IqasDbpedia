ed4454dea1ceee9f6b61a0705094a0a193ddd47c
sunaalkayuniaresta <sunaalkayuni@gmail.com>
2022-12-07 16:26:19 +0700

    minor: refactor code

--- js/hitAPI.js
+++ js/hitAPI.js
@@ -1,12 +1,12 @@
-function show(data) {
-console.log(data)
-document.getElementById("result").innerHTML = data['langkah5']
-}
+// function show(data) {
+// console.log(data)
+// document.getElementById("result").innerHTML = data['langkah5']
+// }
 
- function test(){
-    const pertanyaan =  document.getElementsByClassName("pertanyaan").value;
+// function test(){
+//     const pertanyaan =  document.getElementsByClassName("pertanyaan").value;
 
-console.log( pertanyaan)
+// console.log( pertanyaan)
 
-console.log("BERHASIL TEST AWAL")
-}
+// console.log("BERHASIL TEST AWAL")
+// }
